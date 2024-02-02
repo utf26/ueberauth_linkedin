2cb855607587ff739386804ab081233b2857eddd
Babar Saleh Hayat <babar@vastmesh.com>
2024-02-02 07:31:57 +0500

    updated mix file for publishing package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule UeberauthLinkedIn.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "UeberauthLinkedIn",
+      description: "LinkedIn v2 strategy for Ueberauth.",
+      package: package(),
+      source_url: "https://github.com/utf26/ueberauth_linkedin"
     ]
   end
 
@@ -22,7 +26,17 @@ defmodule UeberauthLinkedIn.MixProject do
   defp deps do
     [
       {:ueberauth, "~> 0.10"},
-      {:oauth2, "~> 2.0"}
+      {:oauth2, "~> 2.0"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Babar Saleh Hayat"],
+      organization: "VastMesh Ltd",
+      licenses: ["MIT", "Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/utf26/ueberauth_linkedin"}
     ]
   end
 end
